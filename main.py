5a1584646fcff5be5b06df182363ee26415f72b0
Gornemant <gornemant@gmail.com>
2025-12-27 12:26:31 +0100

    Ch2 L4 Count Words

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+#Open and read file to return content
 def get_book_text(file_path):
     file_contents = ""
     try:
@@ -8,9 +10,23 @@ def get_book_text(file_path):
         print(f"Error encountered: {e}")
         return "Error opening file."
 
+#Ch3 L4: Count words in book
+def count_words(text):
+    word_list = []
+    number_of_words = 0
+    word_list = text.split()
+    number_of_words = len(word_list)
+    return number_of_words
+
 def main():
     book_content = ""
+    num_words = 0
     book_content = get_book_text("./books/frankenstein.txt")
-    print(book_content)
+
+    #Ch3 L3 print book content
+    #print(book_content)
+
+    num_words = count_words(book_content)
+    print(f"Found {num_words} total words")
 
 main()
