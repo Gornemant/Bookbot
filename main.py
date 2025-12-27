ab3448f89f41ee9d6f147b9b1507ee1fd9171d50
Gornemant <gornemant@gmail.com>
2025-12-27 12:08:06 +0100

    Ch2 L3 Read File (+ error handling wrong file path)

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(file_path):
+    file_contents = ""
+    try:
+        with open(file_path) as f:
+            file_contents = f.read()
+        return file_contents
+    except Exception as e:
+        print(f"Error encountered: {e}")
+        return "Error opening file."
+
+def main():
+    book_content = ""
+    book_content = get_book_text("./books/frankenstein.txt")
+    print(book_content)
+
+main()
