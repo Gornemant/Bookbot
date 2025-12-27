1c3f6520e160a8f4621f23924256088161ab8776
Gornemant <gornemant@gmail.com>
2025-12-27 13:22:02 +0100

    Ch2 L6 Count Characters (+fix wrong chapter in comments)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-#Ch3 L5: Refactor, count_words function moved to stats.py
-from stats import count_words
+#Ch2 L5: Refactor, count_words function moved to stats.py
+from stats import count_words, count_characters
 
 #Open and read file to return content
 def get_book_text(file_path):
@@ -16,13 +16,19 @@ def get_book_text(file_path):
 def main():
     book_content = ""
     num_words = 0
+    num_characters = {}
     book_content = get_book_text("./books/frankenstein.txt")
 
-    #Ch3 L3 print book content
+    #Ch2 L3 print book content
     #print(book_content)
 
-    #Ch3 L4: Count words in book and print how many were found
+    #Ch2 L4: Count words in book and print how many were found
+    #Ch2 L6: Count characters in book and print the result
     num_words = count_words(book_content)
+    num_characters = count_characters(book_content)
     print(f"Found {num_words} total words")
+    for char in num_characters:
+        counted = num_characters[char]
+        print(f"'{char}': {counted}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,19 @@
-#Ch3 L4: Count words in book
+#Ch2 L4: Count words in book
 def count_words(text):
     word_list = []
     number_of_words = 0
     word_list = text.split()
     number_of_words = len(word_list)
     return number_of_words
+
+#Ch2 L5: Count Characters
+def count_characters(text):
+    char_dict = {}
+    #Convert to lower case
+    lc_text = text.lower()
+    for char in lc_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
